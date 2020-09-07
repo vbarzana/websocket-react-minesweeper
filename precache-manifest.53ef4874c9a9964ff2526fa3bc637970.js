612d0dc098f2e5dd01536f8abf2f21b6ca751303
Victor Antonio Barzana Crespo <viticoinf@gmail.com>
2020-09-07 15:30:28 +0000

    Updates

---
+++ precache-manifest.53ef4874c9a9964ff2526fa3bc637970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920f57470c66b219035bf457d561f67a",
+    "url": "/websocket-react-minesweeper/index.html"
+  },
+  {
+    "revision": "59f3d89bde9af7460171",
+    "url": "/websocket-react-minesweeper/static/css/main.58340661.chunk.css"
+  },
+  {
+    "revision": "7b7f7782f8d1230cfc4d",
+    "url": "/websocket-react-minesweeper/static/js/2.c73411b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/websocket-react-minesweeper/static/js/2.c73411b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f3d89bde9af7460171",
+    "url": "/websocket-react-minesweeper/static/js/main.db19f685.chunk.js"
+  },
+  {
+    "revision": "5e8870a1e4ab4560a73a",
+    "url": "/websocket-react-minesweeper/static/js/runtime-main.a552ec87.js"
+  }
+]);
